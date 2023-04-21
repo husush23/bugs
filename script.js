cb97a9014979510fb072714205a360ae1b629cbe
husush23 <114058667+husush23@users.noreply.github.com>
2023-04-21 20:48:07 +0300

    initial commit
    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function hi(){
+    const div  = document.createElement('h1')
+    div.innerHTML = 'Hello, it is great to see you';
+   const el = document.querySelector('.app');
+   el.appendChild(div)
+}
+document.querySelector('button').addEventListener('submit', hi)
